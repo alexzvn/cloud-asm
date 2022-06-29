bf91cdd2925244963df9a0beb975b15724c2fdbe
Alex <41188285+alexzvn@users.noreply.github.com>
2022-06-29 12:32:44 +0000

    fix env

--- src/utils/mongo.js
+++ src/utils/mongo.js
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb"
 
-const dsn = process.env.MONGODB_DSN || "mongodb://localhost:27017"
+const dsn = process.env.MONGO_DSN || "mongodb://localhost:27017"
 
 const database = process.env.MONGODB_DATABASE || "test"
 
